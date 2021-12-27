5383ea66d3e0b464747e021a7644cfc8979b0b87
Lautarograc <lautarograciani2106@gmail.com>
2021-12-27 03:21:54 -0300

    Added post and user models, comments pending

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_27_045525) do
+ActiveRecord::Schema.define(version: 2021_12_27_061306) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "comment"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,4 +28,5 @@ ActiveRecord::Schema.define(version: 2021_12_27_045525) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
